2b436f207dddb974ff64686b297f75dd42999358
albin lindell <albinglindell98@gmail.com>
2025-06-18 08:36:19 +0200

    made the app in to a Progressive Web App (PWA).

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Hailey Bingo",
+        short_name: "Bingo",
+        description: "Company Bingo Game",
+        theme_color: "#ffb2b2",
+        background_color: "#18191a",
+        display: "standalone",
+        start_url: ".",
+        icons: [
+          {
+            src: "favicon/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "favicon/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "favicon/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
